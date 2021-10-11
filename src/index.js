7a5c7198578b69403cc5cca29b9f07f3c02f4747
Amirandard <am.miranda.rd@gmail.com>
2021-10-11 10:53:33 +0200

    checking

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 // create and config server
+
 const server = express();
 server.use(cors());
 server.use(express.json());
-
+server.get("/movies", (req,res)=>{
+//res.
+})
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
