b5ad6f22f9ef4ce57e8bdceef50570dca3fb4b49
Natalia Ramirez <jnataliaramirez@gmail.com>
2021-10-15 11:19:03 +0200

    Do exercise day 5, database I

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
-
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
@@ -15,21 +15,29 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+// init and config data base
+const db = new Database('./src/data/database.db', {
+  // this line log in console all data base queries
+  verbose: console.log,
+});
+
 // API request > GET > http://localhost:4000/movies
 server.get('/movies', (req, res) => {
-  console.log('estoy en la url de movies');
-  res.json(movies);
+  const query = db.prepare(`SELECT * FROM movies`);
+  const moviesData = query.all();
+  res.json(moviesData);
 });
 
 server.get('/movie/:movieId', (req, res) => {
   console.log('Url params:', req.params);
   console.log('Url param promoId:', req.params.movieId);
 
-    // find movie by movieId
-    const movie = movies.movies.find(movie => movie.id === parseInt(req.params.movieId));
-    console.log('Found movie:', movie);
-  
-})
+  // find movie by movieId
+  const movie = movies.movies.find(
+    (movie) => movie.id === parseInt(req.params.movieId)
+  );
+  console.log('Found movie:', movie);
+});
 
 // Configuración del servidor de estáticos
 const staticServerPathWeb = './public';
